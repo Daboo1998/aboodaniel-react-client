ea99ff68de3051b167fa69f032853fcc7ba46feb
Daboo1998 <daboo1998@gmail.com>
2021-06-29 10:46:27 +0200

    Added automatic filling in contact info if logged in.

--- src/components/layouts/pages/ContactPageLayout.tsx
+++ src/components/layouts/pages/ContactPageLayout.tsx
@@ -1,10 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import PageLayout from "../PageLayout";
 import TextInput from "../../atoms/TextInput";
 import TextAreaInput from "../../atoms/TextAreaInput";
 import validator from "validator";
 import firebase from "firebase";
 import "firebase/firestore";
+import {useAuth} from "../../../contexts/AuthContext";
 
 const ContactPageLayout: React.FC = () => {
     const [email, setEmail] = useState("");
@@ -12,13 +13,25 @@ const ContactPageLayout: React.FC = () => {
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
-    const [messageSent, setMessageSent] = useState<"Message sent!" | null>(null);
+    const [information, setInformation] = useState<string | null>(null);
+    const auth = useAuth();
+
+    useEffect(() => {
+        if (auth.isLoggedIn) {
+            setEmail(auth.user?.email ?? "");
+            setName(auth.user?.displayName ?? "");
+
+            setInformation("Some fields were filled in for you. You may edit them if you want.");
+        }
+    }, [
+        auth.isLoggedIn, auth.user
+    ]);
 
     const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault();
       console.log(`${name} (${email}) submitted a message: "${message}"`);
       setErrorMessage(null);
-      setMessageSent(null);
+      setInformation(null);
 
       if (email.length === 0) {
           setErrorMessage("Email is required!");
@@ -38,7 +51,7 @@ const ContactPageLayout: React.FC = () => {
                   email, name, subject, message
               })
               .then(_ => {
-                  setMessageSent("Message sent!");
+                  setInformation("Message sent!");
               })
               .catch(error => {
                   setErrorMessage(error.message);
@@ -59,7 +72,7 @@ const ContactPageLayout: React.FC = () => {
                 <div className="h-6" />
                 <TextAreaInput name="message" value={message} onChange={setMessage}>Message (required)</TextAreaInput>
                 <div className="h-6" />
-                <p className="text-green-600">{ messageSent }</p>
+                <p className="text-green-600">{ information }</p>
                 <p className="text-error bold">{errorMessage}</p>
                 <div className="h-6" />
                 <button type="submit" className="bg-submit text-white rounded py-3 px-5 uppercase">
