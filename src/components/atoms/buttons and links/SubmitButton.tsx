c2382f3cf3f5bd373beea4c4be85eb97a4dca040
Daboo1998 <daboo1998@gmail.com>
2021-07-05 09:25:13 +0100

    Added sending reply as mail and deleting message

--- src/components/atoms/buttons and links/SubmitButton.tsx
+++ src/components/atoms/buttons and links/SubmitButton.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 
+export enum SubmitButtonType {
+    primary = "primary",
+    destructive = "destructive",
+    constructive = "constructive"
+}
+
 export interface SubmitButtonProps {
     label: string;
     onSubmit?: () => void;
+    type?: SubmitButtonType;
 }
 
-const SubmitButton: React.FC<SubmitButtonProps> = ({label, onSubmit}) => (
-    <button type="submit" onClick={_ => onSubmit?.()} className="bg-submit text-white rounded py-3 px-5 uppercase w-full hover:bg-submit-hover">
-        {label}
-    </button>
-);
+const SubmitButton: React.FC<SubmitButtonProps> = ({label, onSubmit, type = SubmitButtonType.primary}) => {
+    const theme = () => {
+        switch (type) {
+            case SubmitButtonType.primary: return "bg-submit hover:bg-submit-hover";
+            case SubmitButtonType.constructive: return "bg-green-600 hover:bg-green-900";
+            case SubmitButtonType.destructive: return "bg-red-600 hover:bg-red-900";
+        }
+    };
+
+    return (
+        <button type="submit" onClick={_ => onSubmit?.()}
+                className={`${theme()} text-white rounded py-3 px-5 uppercase w-full`}>
+            {label}
+        </button>
+    );
+};
 
 export default SubmitButton;
