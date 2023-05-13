fd1c3049a7816aa1d380f65349d137be6b310091
daboo1998 <daboo1998@gmail.com>
2023-05-13 12:23:02 +0200

    fix: fiexed experience dates

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,48 +1,48 @@
-import {Timestamp} from "./database";
+import { Timestamp } from "./database";
 
 export const stringRepresentation = (date: Date, withDay: boolean = false) => {
-    const newDate = new Date(date);
-    const month = newDate.getUTCMonth() + 1;
-    const year = newDate.getUTCFullYear();
+  const newDate = new Date(date);
+  const month = newDate.getUTCMonth() + 1;
+  const year = newDate.getUTCFullYear();
 
-    if (month === 1) {
-        return `January, ${year}`;
-    } else if (month === 2) {
-        return `February, ${year}`;
-    } else if (month === 3) {
-        return `March, ${year}`;
-    } else if (month === 4) {
-        return `April, ${year}`;
-    } else if (month === 5) {
-        return `May, ${year}`;
-    } else if (month === 6) {
-        return `June, ${year}`;
-    } else if (month === 7) {
-        return `July, ${year}`;
-    } else if (month === 6) {
-        return `August, ${year}`;
-    } else if (month === 6) {
-        return `September, ${year}`;
-    } else if (month === 6) {
-        return `October, ${year}`;
-    } else if (month === 6) {
-        return `November, ${year}`;
-    } else if (month === 6) {
-        return `December, ${year}`;
-    }
+  if (month === 1) {
+    return `January, ${year}`;
+  } else if (month === 2) {
+    return `February, ${year}`;
+  } else if (month === 3) {
+    return `March, ${year}`;
+  } else if (month === 4) {
+    return `April, ${year}`;
+  } else if (month === 5) {
+    return `May, ${year}`;
+  } else if (month === 6) {
+    return `June, ${year}`;
+  } else if (month === 7) {
+    return `July, ${year}`;
+  } else if (month === 8) {
+    return `August, ${year}`;
+  } else if (month === 9) {
+    return `September, ${year}`;
+  } else if (month === 10) {
+    return `October, ${year}`;
+  } else if (month === 11) {
+    return `November, ${year}`;
+  } else if (month === 12) {
+    return `December, ${year}`;
+  }
 
-    return "";
+  return "";
 };
 
 interface Experience {
-    id?: string;
-    importance: number,
-    title: string,
-    description: string,
-    startingDate?: Timestamp,
-    endDate?: Timestamp | "ongoing",
-    link?: string,
-    linkText?: string,
+  id?: string;
+  importance: number;
+  title: string;
+  description: string;
+  startingDate?: Timestamp;
+  endDate?: Timestamp | "ongoing";
+  link?: string;
+  linkText?: string;
 }
 
 export default Experience;
