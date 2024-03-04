0275020d0fc2dc0a11d15c8257c549ef6801b567
daboo1998 <daboo1998@gmail.com>
2024-03-04 16:13:03 +0100

    fixed eval is dangerous

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,7 +9,7 @@ export function cx(
     } else if (typeof arg === "object" && arg !== null) {
       Object.entries(arg).forEach(([key, value]) => {
         if (value) {
-          if (typeof value === "string" && eval(value)) {
+          if (typeof value === "string") {
             classes.push(key);
           } else if (typeof value === "boolean" && value) {
             classes.push(key);
