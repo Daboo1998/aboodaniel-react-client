36096924ecb2cc818dd87b49ca3014be0eefccd5
daboo1998 <daboo1998@gmail.com>
2024-03-04 16:34:38 +0100

    Mobile fix for logo

--- src/components/context providers/AuthContextProvider.tsx
+++ src/components/context providers/AuthContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import firebase from "firebase/app";
 import AuthContext from "../../contexts/AuthContext";
 
@@ -8,115 +8,136 @@ import database from "../../data/database";
 const provider = new firebase.auth.GoogleAuthProvider();
 
 const checkForRole = async (role: string, userId: string | undefined) => {
-    if (!userId) {
-        return false;
-    }
+  if (!userId) {
+    return false;
+  }
 
-    try {
-        const roleRes = await database.roles.get(role);
-        console.log(roleRes.users);
+  try {
+    const roleRes = await database.roles.get(role);
+    console.log(roleRes.users);
 
-        return roleRes.users.includes(userId);
-    } catch (error) {
-        console.error(error.message);
-        return false;
-    }
+    return roleRes.users.includes(userId);
+  } catch (error: any) {
+    console.error(error.message);
+    return false;
+  }
 };
 
-export const AuthContextProvider: React.FC = ({children}) => {
-    const [loginSource, setLoginSource] = useState<string>("");
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean| undefined>(undefined);
-    const [user, setUser] = useState<firebase.User | null>(null);
-    const [isDeveloper, setIsDeveloper] = useState(false);
-    const [isOwner, setIsOwner] = useState(false);
+export const AuthContextProvider: React.FC = ({ children }) => {
+  const [loginSource, setLoginSource] = useState<string>("");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean | undefined>(undefined);
+  const [user, setUser] = useState<firebase.User | null>(null);
+  const [isDeveloper, setIsDeveloper] = useState(false);
+  const [isOwner, setIsOwner] = useState(false);
 
-    const checkRoles = async (userId: string | undefined) => {
-        const isDeveloper = await checkForRole("developer", userId);
-        console.log(isDeveloper ? "Is developer" : "Is not a developer");
+  const checkRoles = async (userId: string | undefined) => {
+    const isDeveloper = await checkForRole("developer", userId);
+    console.log(isDeveloper ? "Is developer" : "Is not a developer");
 
-        setIsDeveloper(isDeveloper);
+    setIsDeveloper(isDeveloper);
 
-        const isOwner = await checkForRole("owner", userId);
-        console.log(isOwner ? "Is owner" : "Is not an owner");
+    const isOwner = await checkForRole("owner", userId);
+    console.log(isOwner ? "Is owner" : "Is not an owner");
 
-        setIsOwner(isOwner);
-    };
+    setIsOwner(isOwner);
+  };
 
-    useEffect(() => {
-        const unsubscribe = firebase.auth().onAuthStateChanged(function(newUser) {
-            if (newUser && !newUser.isAnonymous) {
-                setUser(newUser);
-                checkRoles(newUser?.uid).then();
-                setIsLoggedIn(true);
-            } else {
-                setIsLoggedIn(false);
-                setIsOwner(false);
-                setIsDeveloper(false);
-                setUser(null);
-            }
-        });
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged(function (newUser) {
+      if (newUser && !newUser.isAnonymous) {
+        setUser(newUser);
+        checkRoles(newUser?.uid).then();
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+        setIsOwner(false);
+        setIsDeveloper(false);
+        setUser(null);
+      }
+    });
 
-        return () => unsubscribe();
-    }, []);
+    return () => unsubscribe();
+  }, []);
 
-    const handleLogin = async (email: string, password: string, shouldRememberUser: boolean) => {
-        let persistence = firebase.auth.Auth.Persistence.SESSION;
+  const handleLogin = async (
+    email: string,
+    password: string,
+    shouldRememberUser: boolean
+  ) => {
+    let persistence = firebase.auth.Auth.Persistence.SESSION;
 
-        if (shouldRememberUser) {
-            persistence = firebase.auth.Auth.Persistence.LOCAL;
-        }
+    if (shouldRememberUser) {
+      persistence = firebase.auth.Auth.Persistence.LOCAL;
+    }
 
-        return firebase.auth().setPersistence(persistence).then(() => {
-            return firebase.auth().signInWithEmailAndPassword(email, password);
-        });
-    };
+    return firebase
+      .auth()
+      .setPersistence(persistence)
+      .then(() => {
+        return firebase.auth().signInWithEmailAndPassword(email, password);
+      });
+  };
 
-    const handleLoginWithGoogle = async (shouldRememberUser: boolean) => {
-        let persistence = firebase.auth.Auth.Persistence.SESSION;
+  const handleLoginWithGoogle = async (shouldRememberUser: boolean) => {
+    let persistence = firebase.auth.Auth.Persistence.SESSION;
 
-        if (shouldRememberUser) {
-            persistence = firebase.auth.Auth.Persistence.LOCAL;
-        }
+    if (shouldRememberUser) {
+      persistence = firebase.auth.Auth.Persistence.LOCAL;
+    }
 
-        return firebase.auth().setPersistence(persistence).then(() => {
-            return firebase.auth().signInWithPopup(provider);
-        });
-    };
+    return firebase
+      .auth()
+      .setPersistence(persistence)
+      .then(() => {
+        return firebase.auth().signInWithPopup(provider);
+      });
+  };
 
-    const handleRegister = async (email: string, password: string, shouldRememberUser: boolean) => {
-        let persistence = firebase.auth.Auth.Persistence.SESSION;
+  const handleRegister = async (
+    email: string,
+    password: string,
+    shouldRememberUser: boolean
+  ) => {
+    let persistence = firebase.auth.Auth.Persistence.SESSION;
 
-        if (shouldRememberUser) {
-            persistence = firebase.auth.Auth.Persistence.LOCAL;
-        }
+    if (shouldRememberUser) {
+      persistence = firebase.auth.Auth.Persistence.LOCAL;
+    }
 
-        return firebase.auth().setPersistence(persistence).then(() => {
-            return firebase.auth().createUserWithEmailAndPassword(email, password);
-        });
-    };
+    return firebase
+      .auth()
+      .setPersistence(persistence)
+      .then(() => {
+        return firebase.auth().createUserWithEmailAndPassword(email, password);
+      });
+  };
 
-    const handleLogout = () => {
-        return firebase.auth().signOut();
-    };
+  const handleLogout = () => {
+    return firebase.auth().signOut();
+  };
 
-    const wentToLogin = (source: string) => {
-        setLoginSource(source);
-    };
+  const wentToLogin = (source: string) => {
+    setLoginSource(source);
+  };
 
-    return (
-        <AuthContext.Provider value={{
-            user,
-            isLoggedIn,
-            login: handleLogin,
-            loginWithGoogle: handleLoginWithGoogle,
-            logout: handleLogout, wentToLogin,
-            register: handleRegister,
-            loginSource,
-            isDeveloper,
-            isOwner}}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLoggedIn,
+        login: handleLogin,
+        loginWithGoogle: handleLoginWithGoogle,
+        logout: handleLogout,
+        wentToLogin,
+        register: handleRegister,
+        loginSource,
+        isDeveloper,
+        isOwner,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
--- src/components/molecules/general/PageNavigatorBar.tsx
+++ src/components/molecules/general/PageNavigatorBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { ReactComponent as MenuIcon } from "../../../images/icons/menuIcon.svg";
 import { ReactComponent as CloseIcon } from "../../../images/icons/closeIcon.svg";
 import useNavigation from "../../../hooks/useNavigation";
+import { cx } from "../../../utils";
 
 export const PageNavigatorBarContext = React.createContext({
   isHidden: false,
@@ -12,34 +13,38 @@ const PageNavigatorBar: React.FC = ({ children }) => {
   const [isHidden, setIsHidden] = useState(true);
   const navigation = useNavigation();
 
+  const toggleHidden = () => {
+    setIsHidden(!isHidden);
+  };
+
   return (
     <PageNavigatorBarContext.Provider
       value={{ isHidden, hide: () => setIsHidden(true) }}
     >
       <div
-        className={`flex flex-col >md:flex-row ${
-          isHidden ? "" : "border-b <md:h-full"
-        } >md:shadow-md border-black fixed w-full top-0 z-10 `}
+        className={cx(
+          "flex flex-col >md:flex-row >md:shadow-md border-black fixed w-full top-0 z-10",
+          !isHidden && "border-b <md:h-full"
+        )}
       >
         <div className="flex flex-row >md:hidden bg-background-light dark:bg-background-dark w-10 h-14 w-full">
-          <button
-            className="border-0 p-2 w-10 pl-6"
-            onClick={(_) => setIsHidden(!isHidden)}
-          >
+          <button className="border-0 p-2 w-10 pl-6" onClick={toggleHidden}>
             {isHidden ? <MenuIcon width="15px" /> : <CloseIcon width="15px" />}
           </button>
           <h3 className="h-full p-3.5 pl-6">{navigation.currentPageTitle}</h3>
         </div>
         <h3
-          className="whitespace-nowrap self-center p-4 absolute h-full cursor-pointer <md:justify-self-center"
+          className="whitespace-nowrap self-center p-4 absolute h-full cursor-pointer <md:justify-self-center <md:hidden"
           onClick={() => navigation.navigateTo("/")}
         >
           Daniel Aboo
         </h3>
         <div
-          className={`flex flex-col >md:flex-row >md:w-full bg-background-light dark:bg-background-dark >md:items-center >md:justify-center
-              ${isHidden ? "<md:h-0" : "<md:h-screen"}
-              <md:overflow-hidden <md:transition-height <md:duration-500 <md:ease-in-out`}
+          className={cx(
+            "flex flex-col >md:flex-row >md:w-full bg-background-light dark:bg-background-dark >md:items-center >md:justify-center",
+            "<md:overflow-hidden <md:transition-height <md:duration-500 <md:ease-in-out",
+            isHidden ? "<md:h-0" : "<md:h-screen"
+          )}
         >
           {children}
         </div>
